60339412d9fc7ff41e94affb8efb27f7601085a9
xb_grey <xb_grey@163.com>
2018-02-07 15:38:14 +0800

    添加了 antd 和 moment

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
   * react
   * react-router-dom（路由）（等待完成） 
   * react-redux（全局状态管理）（等待完成） 
-  * antd（UI）（等待完成） 
-  * moment（时间）（等待完成）
+  * antd（UI）
+  * moment（时间）
 
 ### 安装
 yarn install
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { TimePicker } from 'antd';
+import moment from 'moment';
 import './App.scss';
 
 const logo = require('./logo.svg')
@@ -11,6 +13,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
+        <TimePicker defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
