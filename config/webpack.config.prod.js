b3113d525e2cfa6aaee6afc25a706eeecd80a23f
xb_grey <xb_grey@163.com>
2018-02-07 16:04:53 +0800

    antd 按需加载

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -149,8 +149,10 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-
               compact: true,
+              plugins: [
+                ['import', { libraryName: 'antd', style: 'css' }],
+              ]
             },
           },
           {
--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ class App extends Component {
         </header>
 
         <TimePicker defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
-
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
